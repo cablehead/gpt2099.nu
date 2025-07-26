0ad761848288808426572e6b03304126f5827570
Andy Gayton <andy@thecablelounge.com>
2025-07-26 13:46:39 +0000

    inline ui

---
+++ ui/src/components/ThemeTrigger.tsx
@@ -0,0 +1,18 @@
+import { Component, Show } from "solid-js";
+import MoonStar from "lucide-solid/icons/moon-star";
+import Sun from "lucide-solid/icons/sun";
+
+const ThemeTrigger: Component<{
+  prefersDark: () => boolean;
+  toggleTheme: () => void;
+}> = (props) => {
+  return (
+    <div class="icon-button" onclick={props.toggleTheme}>
+      <Show when={props.prefersDark()} fallback={<MoonStar />}>
+        <Sun />
+      </Show>
+    </div>
+  );
+};
+
+export default ThemeTrigger;
